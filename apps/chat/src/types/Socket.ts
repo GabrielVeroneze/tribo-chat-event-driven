6d115a1bee5e2d1ac5f762fc3a3cae2bdf5fe907
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-12 16:51:10 -0300

    ✨ feat: adiciona tipagem dos eventos cliente-servidor
    Cria interfaces tipadas para eventos do Socket.IO, padronizando a comunicação entre cliente e servidor e garantindo maior segurança de tipos para mensagens e ações em tempo real.

---
+++ apps/chat/src/types/Socket.ts
@@ -0,0 +1,11 @@
+import type { User } from '@/types/User'
+import type { Message } from '@/types/Message'
+
+export interface ServerToClientEvents {
+    'new-message': (payload: { id: number; newMessage: Message }) => void
+    'read-message': (user: User) => void
+}
+
+export interface ClientToServerEvents {
+    'join-rooms': (chatIds: number[]) => void
+}
