579fe54a446789a88c1f45c158ea57e6e98ff0d8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-13 15:25:08 -0300

    ♻️ refactor: torna callback opcional na função writeFile
    Ajusta a assinatura da função `writeFile` para permitir que o callback seja opcional, tornando a API mais flexível e evitando a necessidade de passar
    funções vazias quando nenhuma ação adicional é necessária após a escrita do arquivo.

--- apps/server/utils/file.ts
+++ apps/server/utils/file.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
-export function writeFile<T>(newDb: T, callback: () => void) {
+export function writeFile<T>(newDb: T, callback?: () => void) {
     fs.writeFile(path.resolve('db.json'), JSON.stringify(newDb, null, 2), () =>
         callback?.(),
     )
